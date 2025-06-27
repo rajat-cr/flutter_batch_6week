0d081809e13f2f30c71296e5ba1e2d20bc7efbe3
rajat-cr <rajat@coderroots.com>
2025-06-27 12:22:36 +0530

    Flutter CLass
    Validation, keyboard type, TextInputAction

--- lib/RowClass.dart
+++ lib/RowClass.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Rowclass extends StatefulWidget {
-  const Rowclass({super.key});
+  String email;
+  Rowclass({required this.email, super.key});
 
   @override
   State<Rowclass> createState() => _RowclassState();
@@ -9,24 +10,34 @@ class Rowclass extends StatefulWidget {
 
 class _RowclassState extends State<Rowclass> {
   @override
+  void initState() {
+    super.initState();
+    print("Email Id: ${widget.email.toString()}");
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Row"), backgroundColor: Colors.purple),
 
-      body: Row(
-        children: [
-          Text("Mansi", style: TextStyle(fontSize: 25)),
-          SizedBox(width: 10),
-          Text("Row Data", style: TextStyle(fontSize: 25)),
-          SizedBox(width: 10),
-          Text("Mansi", style: TextStyle(fontSize: 25)),
-          SizedBox(width: 10),
-          Text("Row Data", style: TextStyle(fontSize: 25)),
-          SizedBox(width: 10),
-          Text("Mansi", style: TextStyle(fontSize: 25)),
-          SizedBox(width: 10),
-          Text("Row Data", style: TextStyle(fontSize: 25)),
-        ],
+      body: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+
+        child: Row(
+          children: [
+            Text("Mansi", style: TextStyle(fontSize: 25)),
+            SizedBox(width: 10),
+            Text("Row Data", style: TextStyle(fontSize: 25)),
+            SizedBox(width: 10),
+            Text("Mansi", style: TextStyle(fontSize: 25)),
+            SizedBox(width: 10),
+            Text("Row Data", style: TextStyle(fontSize: 25)),
+            SizedBox(width: 10),
+            Text("Mansi", style: TextStyle(fontSize: 25)),
+            SizedBox(width: 10),
+            Text("Row Data", style: TextStyle(fontSize: 25)),
+          ],
+        ),
       ),
     );
   }
--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_batch_6week/RowClass.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,6 +9,11 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+  var email = "email@gmail.com";
+  var emailFocus = FocusNode();
+  var passFocus = FocusNode();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +29,10 @@ class _LoginScreenState extends State<LoginScreen> {
             ),
 
             TextField(
+              keyboardType: TextInputType.emailAddress,
+              textInputAction: TextInputAction.next,
+              controller: emailController,
+              focusNode: emailFocus,
               decoration: InputDecoration(
                 hintText: "Enter Your Email",
                 label: Text("E-Mail"),
@@ -31,6 +41,10 @@ class _LoginScreenState extends State<LoginScreen> {
             ),
             SizedBox(height: 10),
             TextField(
+              controller: passwordController,
+              textInputAction: TextInputAction.done,
+              keyboardType: TextInputType.number,
+              focusNode: passFocus,
               decoration: InputDecoration(
                 hintText: "Enter Your Password",
                 label: Text("Password"),
@@ -43,7 +57,50 @@ class _LoginScreenState extends State<LoginScreen> {
               children: [Text("Forget Password ?")],
             ),
 
-            ElevatedButton(onPressed: () {}, child: Text("Login")),
+            ElevatedButton(
+              onPressed: () {
+                if (emailController.text.toString().isEmpty) {
+                  ScaffoldMessenger.of(
+                    context,
+                  ).showSnackBar(SnackBar(content: Text("Enter Email Id")));
+                } else if (passwordController.text.toString().isEmpty) {
+                  ScaffoldMessenger.of(
+                    context,
+                  ).showSnackBar(SnackBar(content: Text("Enter Password")));
+                } else {
+                  setState(() {
+                    // ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Enter Email Id")))
+                    // Fluttertoast.showToast(msg: "Enter Your Password");
+
+                    email = emailController.text.toString();
+                    print(email);
+                  });
+                }
+              },
+              child: Text("Login"),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("You don't have an account?"),
+                SizedBox(width: 5),
+                GestureDetector(
+                  onTap: () {
+                  
+                    Navigator.of(
+                      context,
+                    ).push(MaterialPageRoute(builder: (context) => Rowclass(email: "Mansi@gmail.com",)));
+                  },
+                  child: Text("Sign Up", style: TextStyle(color: Colors.blue)),
+                ),
+
+                // TextButton(
+                //   onPressed: () {},
+                //   child: Text("Sign Up", style: TextStyle(color: Colors.blue)),
+                // ),
+              ],
+            ),
           ],
         ),
       ),
